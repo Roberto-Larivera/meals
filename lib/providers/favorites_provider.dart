077431f6af67d6eae825857e20d7ea1456a6c631
Roberto-Larivera <robylariv@gmail.com>
2024-02-14 22:18:28 +0100

    Aggiunto provider per i pasti preferiti

---
+++ lib/providers/favorites_provider.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meals/models/meal.dart';
+
+class FavoriteMealsNotifier extends StateNotifier<List<Meal>> {
+  FavoriteMealsNotifier() : super([]);
+
+  void toggleMealFavoriteStatus(Meal meal) {
+    final isExisting = state.contains(meal);
+
+    if (isExisting) {
+      // state = List.from(state)..remove(meal);
+      state = state.where((m) => m.id != meal.id).toList();
+    } else {
+      state = [...state, meal];
+    }
+  }
+}
+
+final favoriteMealsProvider =
+    StateNotifierProvider<FavoriteMealsNotifier, List<Meal>>(
+        (ref) => FavoriteMealsNotifier());
